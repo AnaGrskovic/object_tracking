1eb61207fc6875aa34f51267392fcb4bee986a5e
Ana <ana.grskovic56@gmail.com>
2022-06-02 19:51:13 +0200

    Farnebeck but only on selected area.

--- main.py
+++ main.py
@@ -9,11 +9,6 @@ DATA_DIR = "C:/Users/Ana/Desktop/Ana/FER/6.semestar/ZAVRAD/data/"
 
 class MedianFlowTracker(object):
 
-    def __init__(self):
-        self.lk_params = dict(winSize=(11, 11),
-                              maxLevel=3,
-                              criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.1))
-
     def calculate_next_bounding_box(self, frame_1, frame_2, bounding_box_1):
 
         # DRAW A BOUNDING BOX ON FRAME 1
@@ -22,17 +17,22 @@ class MedianFlowTracker(object):
         cv2.imshow("Tracking", frame_1_with_bounding_box)
         cv2.waitKey(0)
 
+        bounding_box_1_left = bounding_box_1[0]
+        bounding_box_1_top = bounding_box_1[1]
+        bounding_box_1_width = bounding_box_1[2]
+        bounding_box_1_height = bounding_box_1[3]
+        cropped_frame_1 = frame_1[bounding_box_1_top : bounding_box_1_top + bounding_box_1_height, bounding_box_1_left : bounding_box_1_left + bounding_box_1_width]
+        cropped_frame_2 = frame_2[bounding_box_1_top: bounding_box_1_top + bounding_box_1_height, bounding_box_1_left: bounding_box_1_left + bounding_box_1_width]
+
         # CALCULATE FORWARD OPTICAL FLOW
-        frame_1_gray = cv2.cvtColor(frame_1, cv2.COLOR_RGB2GRAY)
-        frame_2_gray = cv2.cvtColor(frame_2, cv2.COLOR_RGB2GRAY)
+        cropped_gray_frame_1 = cv2.cvtColor(cropped_frame_1, cv2.COLOR_RGB2GRAY)
+        cropped_gray_frame_2 = cv2.cvtColor(cropped_frame_2, cv2.COLOR_RGB2GRAY)
 
         flow = None
-        flow = cv2.calcOpticalFlowFarneback(prev=frame_1_gray, next=frame_2_gray, flow=flow, pyr_scale=0.8,
+        flow = cv2.calcOpticalFlowFarneback(prev=cropped_gray_frame_1, next=cropped_gray_frame_2, flow=flow, pyr_scale=0.8,
                                             levels=15, winsize=5, iterations=10, poly_n=5, poly_sigma=0, flags=10)
-        # magnitude, radian_angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-        # pi = 22 / 7
-        # degree_angle = [rad*(180/pi) for rad in radian_angle]
-        h, w = frame_2.shape[:2]
+
+        h, w = cropped_gray_frame_2.shape[:2]
         step = 16
         y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2, -1).astype(int)
         fx, fy = flow[y, x].T
@@ -65,8 +65,8 @@ class MedianFlowTracker(object):
 if __name__ == '__main__':
     tracker = MedianFlowTracker()
 
-    frame1 = cv2.imread(DATA_DIR + "walking1.png")
-    frame2 = cv2.imread(DATA_DIR + "walking2.png")
+    frame1 = cv2.imread(DATA_DIR + "birds1.png")
+    frame2 = cv2.imread(DATA_DIR + "birds2.png")
 
     bbox1 = cv2.selectROI(frame1, False)
     cv2.destroyAllWindows()
